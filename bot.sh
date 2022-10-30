ba079cef0b351493c8c1e3d04baa27d1a8d845ce
Bruno Gois Costa <53541903+BrunoCosta1303@users.noreply.github.com>
2022-10-30 18:56:46 -0300

    Add files via upload
    Please note that the config file is a batch with the list of commands to configure server/container used.

---
+++ bot.sh
@@ -0,0 +1,90 @@
+ 1  id
+    2  cat passwd
+    3  cat /etc/passwd
+    4  sudo -l
+    5  clear
+    6  adduser augustus
+    7  sudo adduser augustus
+    8  sudo passwd augustus
+    9  sudo apt update
+   10  yum
+   11  clear
+   12  sudo yum update
+   13  sudo yum upgrade
+   14  sudo yum install nodejs
+   15  
+   16  sudo yum install -y gcc-c++ make
+   17  curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+   18  sudo yum install nodejs rpm
+   19  sudo yum install nodejs npm
+   20  npm -versio
+   21  sudo yum install ffmpeg
+   22  cd /usr/local/bin
+   23  mkdir ffmpeg && cd ffmpeg
+   24  sudo mkdir ffmpeg && cd ffmpeg
+   25  uname -a
+   26  sudo yum install wget
+   27  wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+   28  sudo wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+   29  ls
+   30  sudo tar 0xf ffmpeg-release-amd64-static.tar.xz
+   31  sudo tar -xf ffmpeg-release-amd64-static.tar.xz
+   32  cd ffmpeg-4.4-amd64-static/
+   33  ls
+   34  ./ffmpeg -version
+   35  cd ~
+   36  ffmpeg
+   37  sudo cp -a /usr/local/bin/ffmpeg/ffmpeg-4.2.1-amd64-static/ . /usr/local/bin/ffmpeg/
+   38  cp -a /usr/local/bin/ffmpeg/ffmpeg-4.4-amd64-static/ ./usr/local/bin/ffmpeg/
+   39  sudo cp -a /usr/local/bin/ffmpeg/ffmpeg-4.4-amd64-static/ ./usr/local/bin/ffmpeg/
+   40  sudo mkdir /usr/local/bin/ffmpeg
+   41  sudo mkdir /usr/local/bin/ffmpeg/
+   42  ls /usr/local
+   43  sudo cp -a /usr/local/bin/ffmpeg/ffmpeg-4.4-amd64-static/ . /usr/local/bin/ffmpeg/
+   44  cd /usr/local/bin/ffmpeg/
+   45  ls
+   46  rm -rf ffmpeg-release-amd64-static.tar.xz
+   47  ls
+   48  cd ffmpeg-4.4-amd64-static/
+   49  ls
+   50  cp -h
+   51  cp --help
+   52  sudo cp -a /usr/local/bin/ffmpeg/ffmpeg-4.4-amd64-static/ . /usr/local/bin/ffmpeg/
+   53  ls
+   54  mv ffmpeg ../
+   55  mv ffprobe ../
+   56  mv GPLv3.txt ../
+   57  mv manages ../
+   58  mv manpages ../
+   59  mv manpages/ ../
+   60  mvdir manpages/ ../
+   61  mv manpages/ ../
+   62  mv manpages/ ../../
+   63  sudo mv manpages/ ../../
+   64  ls
+   65  sudo mv model/ ../../
+   66  sudo mv qt-faststart  ../
+   67  sudo mv readme.txt ../
+   68  cd ..
+   69  ls
+   70  rm -rf ffmpeg-4.4-amd64-static/
+   71  clear
+   72  ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+   73  sudo ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+   74  ffmpeg
+   75  su augustus
+   76  sudo yum install git
+   77  su augustus
+   78  npm install -g npm@7.22.0
+   79  sudo npm install -g npm@7.22.0
+   80  su augustus
+   81  ls
+   82  cd augustus
+   83  su augustus
+   84  ls
+   85  sudo yum update -y && yum upgrade
+   86  clear
+   87  su augustus
+   88  cat /etc/passwd
+   89  su augustus
+   90  history
